da27a2537be54b76da9595f57e1a6ea61df4acfa
tamvv <vovantam.dev@gmail.com>
2021-03-27 15:39:54 +0700

    Add email sender

--- src/main/java/io/spring/app/utils/SendEmailImpl.java
+++ src/main/java/io/spring/app/utils/SendEmailImpl.java
@@ -1,16 +1,46 @@
 package io.spring.app.utils;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 @Log4j2
 @Component
 public class SendEmailImpl implements SendEmail {
 
+    @Value("${email.username}")
+    private String username;
+
+    @Value("${email.password}")
+    private String password;
+
     @Override
     public void send(String email, String title, String content) {
-        String logInfo = String.format("Email: %s, title: %s, content: %s", email, title, content);
-        log.info(logInfo);
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.port", "25");
+
+        Session session = Session.getDefaultInstance(prop);
+        Message message = new MimeMessage(session);
+
+        try {
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            message.setSubject(title);
+            message.setText(content);
+
+            Transport transport = session.getTransport("smtp");
+            transport.connect("smtp.gmail.com", username, password);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
